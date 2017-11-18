b55eef3f92ee49bfda0db266ac2ce8c42621398a
Günther Foidl <gue@korporal.at>
2017-11-18 13:24:39 +0100

    Tolerance when no timings are available in the trx-results
    For instance due to explicit tests, that are not run.

--- source/trx2junit/Models/Times.cs
+++ source/trx2junit/Models/Times.cs
@@ -4,11 +4,11 @@ namespace trx2junit
 {
     public class Times
     {
-        public DateTime Creation { get; set; }
-        public DateTime Queuing  { get; set; }
-        public DateTime Start    { get; set; }
-        public DateTime Finish   { get; set; }
+        public DateTime? Creation { get; set; }
+        public DateTime? Queuing  { get; set; }
+        public DateTime? Start    { get; set; }
+        public DateTime? Finish   { get; set; }
         //---------------------------------------------------------------------
-        public TimeSpan RunTime => this.Finish - this.Start;
+        public TimeSpan? RunTime => this.Finish - this.Start;
     }
 }
--- source/trx2junit/Models/UnitTestResult.cs
+++ source/trx2junit/Models/UnitTestResult.cs
@@ -4,14 +4,14 @@ namespace trx2junit
 {
     public class UnitTestResult
     {
-        public Guid     ExecutionId { get; set; }
-        public Guid     TestId      { get; set; }
-        public string   TestName    { get; set; }
-        public TimeSpan Duration    { get; set; }
-        public DateTime StartTime   { get; set; }
-        public DateTime EndTime     { get; set; }
-        public Outcome  Outcome     { get; set; }
-        public string   Message     { get; set; }
-        public string   StackTrace  { get; set; }
+        public Guid      ExecutionId { get; set; }
+        public Guid      TestId      { get; set; }
+        public string    TestName    { get; set; }
+        public TimeSpan? Duration    { get; set; }
+        public DateTime? StartTime   { get; set; }
+        public DateTime? EndTime     { get; set; }
+        public Outcome   Outcome     { get; set; }
+        public string    Message     { get; set; }
+        public string    StackTrace  { get; set; }
     }
 }
