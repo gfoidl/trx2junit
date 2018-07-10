886213968c36a9718aac0d810803f3abccf09b87
Günther Foidl <gue@korporal.at>
2018-07-10 11:49:38 +0200

    MsTestSample added

---
+++ samples/MsTestSample/SimpleTests.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MsTestSample
+{
+    [TestClass]
+    public class SimpleTests
+    {
+        [TestMethod]
+        public void Passing_test()
+        {
+        }
+        //---------------------------------------------------------------------
+        [TestMethod]
+        public void Failing_test()
+        {
+            // This will cause the CI-test-scripts to fail the build.
+            // So use 'set +e' in the test-scripts to avoid the fail -- here
+            // it is just for demonstration.
+            Assert.Fail("Failing for demo purposes");
+        }
+        //---------------------------------------------------------------------
+        [TestMethod]
+        public async Task Slow_test()
+        {
+            await Task.Delay(1000);
+        }
+    }
+}
