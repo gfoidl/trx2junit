67183b387978f2da7a2a0100c23293fde4616a72
Günther Foidl <gue@korporal.at>
2018-09-12 15:10:00 +0200

    Unit-tests validate against junit.xsd

---
+++ tests/trx2junit.Tests/ValidationHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace trx2junit.Tests
+{
+    internal static class ValidationHelper
+    {
+        private static readonly XmlSchemaSet s_schema;
+        //---------------------------------------------------------------------
+        static ValidationHelper()
+        {
+            s_schema = new XmlSchemaSet();
+
+            using (Stream s = File.OpenRead("./data/junit.xsd"))
+            using (XmlReader xr = XmlReader.Create(s))
+                s_schema.Add("", xr);
+        }
+        //---------------------------------------------------------------------
+        public static void IsXmlValidJunit(string fileName)
+        {
+            XDocument junit = XDocument.Load(fileName);
+
+            junit.Validate(s_schema, (o, e) => throw e.Exception);
+        }
+    }
+}
