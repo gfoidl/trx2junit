b72e8bffdd8963769d9dbc98147913cdf4d88a14
Günther Foidl <gue@korporal.at>
2019-04-17 22:09:44 +0200

    JUnitBuilderTests/Integration-Test added

---
+++ tests/trx2junit.Tests/Internal/JUnitBuilderTests/Integration.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+using NUnit.Framework;
+
+namespace trx2junit.Tests.Internal.JUnitBuilderTests
+{
+    [TestFixture]
+    public class Integration
+    {
+        [Test]
+        [TestCase("./data/mstest-datadriven.trx")]
+        [TestCase("./data/nunit-datadriven.trx")]
+        [TestCase("./data/xunit-datadriven.trx")]
+        public void File_given___correct_test_count(string trxFile)
+        {
+            XElement trx = XElement.Load(trxFile);
+            var parser   = new TrxParser(trx);
+
+            parser.Parse();
+            Models.Test testData = parser.Result;
+
+            var sut = new JUnitBuilder(testData);
+            sut.Build();
+            XElement testsuite = sut.Result.Elements("testsuite").First();
+
+            Assert.AreEqual(5, int.Parse(testsuite.Attribute("tests").Value));
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        [TestCase("./data/mstest-datadriven.trx")]
+        [TestCase("./data/nunit-datadriven.trx")]
+        [TestCase("./data/xunit-datadriven.trx")]
+        public void File_given___correct_failure_count(string trxFile)
+        {
+            XElement trx = XElement.Load(trxFile);
+            var parser   = new TrxParser(trx);
+
+            parser.Parse();
+            Models.Test testData = parser.Result;
+
+            var sut = new JUnitBuilder(testData);
+            sut.Build();
+            XElement testsuite = sut.Result.Elements("testsuite").First();
+
+            Assert.AreEqual(2, int.Parse(testsuite.Attribute("failures").Value));
+        }
+    }
+}
