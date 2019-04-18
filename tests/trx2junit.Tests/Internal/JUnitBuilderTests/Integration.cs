49cea16ea84164564cd16826cdc00521ca5376c3
Günther Foidl <gue@korporal.at>
2019-04-18 18:54:01 +0200

    Integration-test for JUnitBuilder simplified and it uses all test-cases for trx-files

--- tests/trx2junit.Tests/Internal/JUnitBuilderTests/Integration.cs
+++ tests/trx2junit.Tests/Internal/JUnitBuilderTests/Integration.cs
@@ -8,10 +8,15 @@ namespace trx2junit.Tests.Internal.JUnitBuilderTests
     public class Integration
     {
         [Test]
-        [TestCase("./data/mstest-datadriven.trx", Ignore = "https://github.com/gfoidl/trx2junit/issues/43")]
-        [TestCase("./data/nunit-datadriven.trx")]
-        [TestCase("./data/xunit-datadriven.trx")]
-        public void File_given___correct_test_count(string trxFile)
+        [TestCase("./data/mstest.trx"           , 3, 1)]
+        [TestCase("./data/mstest-datadriven.trx", 5, 2)]
+        [TestCase("./data/nunit.trx"            , 3, 1)]
+        [TestCase("./data/nunit-datadriven.trx" , 5, 2)]
+        [TestCase("./data/nunit-memberdata.trx" , 5, 2)]
+        [TestCase("./data/xunit.trx"            , 3, 1)]
+        [TestCase("./data/xunit-datadriven.trx" , 5, 2)]
+        [TestCase("./data/xunit-memberdata.trx" , 5, 2)]
+        public void File_given___correct_counts(string trxFile, int expectedTestCount, int expectedFailureCount)
         {
             XElement trx = XElement.Load(trxFile);
             var parser   = new TrxParser(trx);
@@ -23,26 +28,11 @@ namespace trx2junit.Tests.Internal.JUnitBuilderTests
             sut.Build();
             XElement testsuite = sut.Result.Elements("testsuite").First();
 
-            Assert.AreEqual(5, int.Parse(testsuite.Attribute("tests").Value));
-        }
-        //---------------------------------------------------------------------
-        [Test]
-        [TestCase("./data/mstest-datadriven.trx", Ignore = "https://github.com/gfoidl/trx2junit/issues/43")]
-        [TestCase("./data/nunit-datadriven.trx")]
-        [TestCase("./data/xunit-datadriven.trx")]
-        public void File_given___correct_failure_count(string trxFile)
-        {
-            XElement trx = XElement.Load(trxFile);
-            var parser   = new TrxParser(trx);
-
-            parser.Parse();
-            Models.Test testData = parser.Result;
-
-            var sut = new JUnitBuilder(testData);
-            sut.Build();
-            XElement testsuite = sut.Result.Elements("testsuite").First();
-
-            Assert.AreEqual(2, int.Parse(testsuite.Attribute("failures").Value));
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(expectedTestCount   , int.Parse(testsuite.Attribute("tests").Value)   , nameof(expectedTestCount));
+                Assert.AreEqual(expectedFailureCount, int.Parse(testsuite.Attribute("failures").Value), nameof(expectedFailureCount));
+            });
         }
     }
 }
