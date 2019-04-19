42abd0f0c51ba438cd722f7bb531ebd1e48394c3
Günther Foidl <gue@korporal.at>
2019-04-19 23:07:55 +0200

    Check the number of converted files in globbing-test

--- tests/scripts/run-globbing.sh
+++ tests/scripts/run-globbing.sh
@@ -11,3 +11,17 @@ echo ""
 for junit in ./globbing/*.xml; do
     ./verify-xml.sh "$junit"
 done
+
+nTrx=$(ls -l ./globbing/*.trx | wc -l)
+nXml=$(ls -l ./globbing/*.xml | wc -l)
+
+echo ""
+echo "Count of trx-files: $nTrx"
+echo "Count of xml-files: $nXml"
+
+if [[ $nTrx != $nXml ]]; then
+    echo "FAILURE: not all trx-files were converted"
+    exit 1
+else
+    echo "all trx-files were converted"
+fi
