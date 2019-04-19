51e2d447da31995b185e9d5574559c10161bab22
Günther Foidl <gue@korporal.at>
2019-04-19 14:16:52 +0200

    Nit Spacing in MemberData

--- samples/NUnitSample/MemberData.cs
+++ samples/NUnitSample/MemberData.cs
@@ -4,37 +4,37 @@ using NUnit.Framework;
 
 namespace NUnitSample
 {
-	public class MemberData
-	{
-		private static IEnumerable<object[]> TestData = new[]
-		{
-			new object[] { new { index = 0 } },
-			new object[] { new { index = 1 } },
-			new object[] { new { index = 2 } }
-		};
-		//---------------------------------------------------------------------
-		[Test, TestCaseSource(nameof(TestData))]
-		public void Two_pass_one_fails(dynamic obj)
-		{
-			if (obj.index % 2 == 0)
-				Assert.True(true);
-			else
-				Assert.True(false, "Failing for demo purposes");
-		}
-		//---------------------------------------------------------------------
-		[Test]
-		public void Failing_test()
-		{
-			// This will cause the CI-test-scripts to fail the build.
-			// So use 'set +e' in the test-scripts to avoid the fail -- here
-			// it is just for demonstration.
-			Assert.True(false, "Failing for demo purposes");
-		}
-		//---------------------------------------------------------------------
-		[Test]
-		public Task Slow_test()
-		{
-			return Task.Delay(1000);
-		}
-	}
+    public class MemberData
+    {
+        private static IEnumerable<object[]> TestData = new[]
+        {
+            new object[] { new { index = 0 } },
+            new object[] { new { index = 1 } },
+            new object[] { new { index = 2 } }
+        };
+        //---------------------------------------------------------------------
+        [Test, TestCaseSource(nameof(TestData))]
+        public void Two_pass_one_fails(dynamic obj)
+        {
+            if (obj.index % 2 == 0)
+                Assert.True(true);
+            else
+                Assert.True(false, "Failing for demo purposes");
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        public void Failing_test()
+        {
+            // This will cause the CI-test-scripts to fail the build.
+            // So use 'set +e' in the test-scripts to avoid the fail -- here
+            // it is just for demonstration.
+            Assert.True(false, "Failing for demo purposes");
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        public Task Slow_test()
+        {
+            return Task.Delay(1000);
+        }
+    }
 }
