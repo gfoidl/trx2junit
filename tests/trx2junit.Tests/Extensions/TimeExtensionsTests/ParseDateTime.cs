571888528609443f0785ab0a307b267d9716af2b
Günther Foidl <gue@korporal.at>
2019-11-16 18:20:45 +0100

    Brute_force_validation for ParseDateTime
    Cf. https://github.com/gfoidl/trx2junit/pull/64#discussion_r347099520

--- tests/trx2junit.Tests/Extensions/TimeExtensionsTests/ParseDateTime.cs
+++ tests/trx2junit.Tests/Extensions/TimeExtensionsTests/ParseDateTime.cs
@@ -22,6 +22,34 @@ namespace trx2junit.Tests.Extensions.TimeExtensionsTests
         }
         //---------------------------------------------------------------------
         [Test]
+        public void Brute_force_validation()
+        {
+            int year  = 2019;
+            int month = 11;
+            int day   = 16;
+
+            for (int hour = 0; hour < 24; ++hour)
+            {
+                for (int minute = 0; minute < 60; ++minute)
+                {
+                    for (int second = 0; second < 60; ++second)
+                    {
+                        DateTime dt  = new DateTime(year, month, day, hour, minute, second);
+                        string value = dt.ToJUnitDateTime();
+
+                        DateTime? actual = value.ParseDateTime();
+
+                        Assert.Multiple(() =>
+                        {
+                            Assert.IsTrue(actual.HasValue);
+                            Assert.AreEqual(dt, actual.Value, "failure at {0}", dt);
+                        });
+                    }
+                }
+            }
+        }
+        //---------------------------------------------------------------------
+        [Test]
         public void Valid_TrxDateTime___OK()
         {
             DateTime now = new DateTime(2019, 11, 10, 15, 33, 27, 123);
